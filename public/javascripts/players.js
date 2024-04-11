54a4a80a2f19137cf45566fdd56298f281e0ebbb
Marco Schiavone <marko.skiaa40@gmail.com>
2024-04-11 22:00:31 +0200

    players part made (NOTE: bug on chat)

--- public/javascripts/players.js
+++ public/javascripts/players.js
@@ -1,3 +1,6 @@
+
+/** Function used to generate and display players,
+ * triggered when the **searchBar** is used in the _player.html_ page. */
 function searchPlayer(event) {
     document.getElementById('submitPlayerForm').disabled = true;
     let formData = extractFormData("searchPlayer");
@@ -8,32 +11,32 @@ function searchPlayer(event) {
             method: 'get'
         })
             .then(data => {
-                console.log(data);
                 let dataResponse = Array(data.data)[0];
-                console.log(JSON.stringify(dataResponse));
                 document.getElementsByClassName('body-bg').item(0).style.background = 'none';
                 document.getElementById('form-div').classList.add('d-none');
                 let elements = document.getElementsByClassName('player-search-hide');
-                for (let i = 0; i < elements.length; i++) {
-                    elements.item(i).classList.remove('d-none');
-                }
-                document.getElementById('playersList').classList.remove('d-none');
-                document.getElementById('playerContentFlex').classList.remove('align-items-center');
-                document.getElementById('playerContentFlex').classList.remove('justify-content-center');
-                document.getElementById('playerContentFlex').classList.add('col-lg-9');
-                const cardContainer = document.getElementById('cardContainer');
+                for (let el of elements)
+                    el.classList.remove('d-player-none');
+                let contentDiv = document.getElementById('playerContentFlex');
+                contentDiv.classList.add('col-lg-9');
+                contentDiv.classList.remove('align-items-center');
+                let playerList = document.getElementById('playersList')
+                playerList.parentElement.classList.remove('d-none');
                 dataResponse.forEach((player) => {
-                    const cardElement = document.createElement('div');
-                    cardElement.classList.add('mb-3');
-
-                    cardElement.innerHTML = '<div class="card">' +
-                        '<img src="'+player.imageUrl+'" class="card-img-top" alt="immagine">' +
-                        '                            <div class="card-body">' +
-                        '                                <h5 class="card-title">'+player.playerName+'</h5>' +
-                        '                                <a href="#" class="btn btn-primary">See profile</a>' +
-                        '                            </div>' +
-                        '                        </div>';
-                    cardContainer.appendChild(cardElement);
+                    const playerContainer = document.createElement('div');
+                    playerContainer.classList.add('col-8', 'col-6', 'col-sm-3', 'col-md-2', 'justify-content-center',
+                        'align-items-center', 'mb-4');
+                    let clickableContent = document.createElement('a');
+                    clickableContent.href = 'single_page/player/' + String(player.playerId);
+                    clickableContent.classList.add('text-dark');
+                    clickableContent.innerHTML =
+                        '<img src="'+player.imageUrl+'" class="img-fluid d-block border border-5 ' +
+                            'border-darkgreen rounded-4 player-img-size" alt="image not found"/>' +
+                        '<div class="d-flex justify-content-center align-items-center w-100 my-2 p-0">' +
+                        '   <span class="h6 text-center p-0">' + player.playerName + '</span>' +
+                        '</div>';
+                    playerContainer.appendChild(clickableContent)
+                    playerList.appendChild(playerContainer);
                 })
             })
             .catch(err => console.log(err));
