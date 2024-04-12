4073868c607780e66a1c69d6daad997a47f70cfb
Ivan <ivan.lusso@edu.unito.it>
2024-04-12 14:23:23 +0200

    minor changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -28,17 +28,12 @@ function initPlayers(){
 
 /** Function called by the **main** *"init"* functions to set common attributes and features. */
 function commonInitOfPage() {
-    addBtnFunctions();
-    initChat();
+    // todo: check if can still be useful
 }
 
 /** This function assigns **ALL** the *'onclick'* attributes in the page. */
 function addBtnFunctions() {
-    document.getElementById('chatIconBtn').onclick = clickChatBtn;
-    document.getElementById('closeChat').onclick = closeChat;
-    document.getElementById('acceptTermsBtn').onclick = acceptedTerms;
-    document.getElementById('declineTermsBtn').onclick = closeChat;
-    document.getElementById("submitForm").onclick =  connectToRoom;
+    //todo: fill with index buttons
 }
 
 /** This is an EXPRESS GET function:
