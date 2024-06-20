d2107099e8e92bf8eef528354f7fc8b50218e22d
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2024-06-20 14:18:47 +0200

    Added javadocs to clubs.js function

--- public/javascripts/clubs.js
+++ public/javascripts/clubs.js
@@ -14,63 +14,16 @@ async function initClubs() {
     document.getElementById('submitClubForm').addEventListener('click', searchClubs);
 }
 
-/** This function creates an HTML element with the following structure:
- * ```
- * <div class="accordion-item rounded-1 mb-1">
- *     <h2 class="accordion-header">
- *         <button class="accordion-button custom-accordion collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_1" aria-expanded="false" aria-controls="accordion_1">
- *             <img src="" class="img me-2 custom-rounded-0_5" style="height: 1.2rem"/>
- *             <span>Accordion Item</span>
- *         </button>
- *     </h2>
- *     <div id="accordion_1" class="accordion-collapse collapse" data-bs-parent="#clubAccordion">
- *         <div class="accordion-body">
- *             # content inside
- *         </div>
- *     </div>
- * </div>
- * ```
- * @param fatherId {string} is the accordion *id* to which bind the accordion-item to
- * @param localCompetitionCode {string} is the code used to bind the button to the country */
-
-/*function createAccordion(fatherId, localCompetitionCode) {
-    let wrapperDiv = document.createElement('div');
-    wrapperDiv.classList.add('accordion-item', 'rounded-1', 'mb-1');
-    let header = document.createElement('h2');
-    header.classList.add('accordion-header');
-    wrapperDiv.appendChild(header);
-    let accordionButton = document.createElement('button');
-    accordionButton.classList.add('accordion-button', 'custom-accordion', 'collapsed');
-    accordionButton.type = "button";
-    accordionButton.setAttribute('data-bs-toggle', 'collapse')
-    accordionButton.setAttribute('aria-expanded', 'false');
-    accordionButton.setAttribute('aria-controls', '#' + String(localCompetitionCode));
-    accordionButton.setAttribute('data-bs-target', '#' + String(localCompetitionCode));
-    accordionButton.addEventListener('click', openAccordionClubs.bind(null, localCompetitionCode));
-    header.appendChild(accordionButton);
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('img', 'me-2', 'custom-rounded-0_5');
-    flagImg.src = getFlagOf(localCompetitionCode);
-    flagImg.style.height = '1.2rem';
-    accordionButton.appendChild(flagImg);
-    let spanTitle = document.createElement('span');
-    spanTitle.innerText = getNationNameOf(localCompetitionCode);
-    accordionButton.appendChild(spanTitle);
-    let collapseDiv = document.createElement('div');
-    collapseDiv.classList.add('accordion-collapse', 'collapse');
-    collapseDiv.setAttribute('data-bs-parent', '#' + String(fatherId));
-    collapseDiv.id = String(localCompetitionCode);
-    wrapperDiv.appendChild(collapseDiv);
-    let accBody = document.createElement('div');
-    accBody.classList.add('accordion-body');
-    collapseDiv.appendChild(accBody);
-    document.getElementById(fatherId).appendChild(wrapperDiv);
-}*/
-
+/**Function to get the URL to retrieve a nation flag image with a specified **competition code**
+ *
+ * @param localCompetitionCode The code that identifies a competition nation*/
 function getFlagOf(localCompetitionCode) {
     return flags.get(localCompetitionCode).flagURL;
 }
 
+/**Function to retrieve the name of a country with a specified **competition code**
+ *
+ * @param localCompetitionCode The code that identifies a competition nation*/
 function getNationNameOf(localCompetitionCode) {
     return flags.get(localCompetitionCode).countryName;
 }
@@ -118,7 +71,9 @@ async function openAccordionClubs(id) {
 }
 
 /** It sends a GET extracting the form data and retrieving a club list as an {@link array}.
- * @param event - event fired when the form is submitted. */
+ *
+ * @param event - event fired when the form is submitted.
+ * */
 function searchClubs(event) {
     document.getElementById('submitClubForm').disabled = true;
     let formData = extractFormData('searchClub', false);
